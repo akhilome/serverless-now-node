b96382018cfe027f3039278b250ae77f4556d400
Kizito Akhilome <hovkard@gmail.com>
2019-07-28 12:13:09 +0100

    trigger auto deployment

--- api/v1/index.ts
+++ api/v1/index.ts
@@ -1,8 +1,8 @@
 import { NowRequest, NowResponse } from '@now/node';
 
-export default (req: NowRequest, res: NowResponse) => {
+export default (req: NowRequest, res: NowResponse): NowResponse => {
   return res.status(200).json({
     success: true,
-    message: 'API v1 working alright 😀'
+    message: 'API v1 working alright! 😀'
   });
 };
