916355ff6451b758f8350e08dc62d7d45465a713
Kizito Akhilome <hovkard@gmail.com>
2019-07-28 12:10:47 +0100

    add base api/v1 endpoint & handler

---
+++ api/v1/index.ts
@@ -0,0 +1,8 @@
+import { NowRequest, NowResponse } from '@now/node';
+
+export default (req: NowRequest, res: NowResponse) => {
+  return res.status(200).json({
+    success: true,
+    message: 'API v1 working alright 😀'
+  });
+};
