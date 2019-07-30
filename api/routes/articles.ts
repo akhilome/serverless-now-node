0df6221962675d7ed2aa11f1e1662452305ddb35
Kizito Akhilome <hovkard@gmail.com>
2019-07-30 11:23:33 +0100

    implement functionality to update an existing article

--- api/routes/articles.ts
+++ api/routes/articles.ts
@@ -1,16 +1,16 @@
 import { NowRequest, NowResponse } from '@now/node';
 import ArticleController from '../controllers/Article';
 
+// matches /api/v1/articles/<mongo_objectid>
+const singleArticlePath = new RegExp(/^\/\w{3}\/\w{2}\/\w{8}\/\w{24}\/?$/);
+// matches /api/v1/articles
+const allArticlesPath = new RegExp(/^\/\w{3}\/\w{2}\/\w{8}\/?$/);
+
 const handleGetRequests = async (
   requestUrl: string,
   req: NowRequest,
   res: NowResponse
 ): Promise<NowResponse> => {
-  // matches /api/v1/articles/<mongo_objectid>
-  const singleArticlePath = new RegExp(/^\/\w{3}\/\w{2}\/\w{8}\/\w{24}\/?$/);
-  // matches /api/v1/articles
-  const allArticlesPath = new RegExp(/^\/\w{3}\/\w{2}\/\w{8}\/?$/);
-
   if (singleArticlePath.test(requestUrl)) {
     const singleArticleResponse = await ArticleController.getArticle(req, res);
     return singleArticleResponse;
@@ -41,6 +41,19 @@ export default async (
     case 'GET':
       const getArticlesResponse = await handleGetRequests(url, req, res);
       return getArticlesResponse;
+
+    case 'PUT':
+      if (!singleArticlePath.test(url)) {
+        return res.status(400).json({
+          success: false,
+          message: 'Invalid route'
+        });
+      }
+      const updatedArticleResponse = await ArticleController.updateArticle(
+        req,
+        res
+      );
+      return updatedArticleResponse;
 
     default:
       return res.status(404).json({
