f3254c4b1aed3c25877e5ff16f4bd75a3bfc5c54
Kizito Akhilome <hovkard@gmail.com>
2019-08-06 15:50:14 +0100

    test: add tests for GET /articles

---
+++ tests/__mocks__/articles.ts
@@ -0,0 +1,23 @@
+const articles: {
+  title: string;
+  author: string;
+  body: string;
+}[] = [
+  {
+    title: 'First Post',
+    author: 'Tester',
+    body: "This is the article's body"
+  },
+  {
+    title: 'Second Post',
+    author: 'Tester',
+    body: "This is the article's body"
+  },
+  {
+    title: 'Third Post',
+    author: 'Tester',
+    body: "This is the article's body"
+  }
+];
+
+export default articles;
